a8256a9f0d0845f032b376e6e91ded802f8e254a
cyk <1076504979@qq.com>
2022-06-10 15:46:42 +0800

    resubmit

---
+++ run_MVTec.sh
@@ -0,0 +1,16 @@
+gpu_id=3
+python train.py --gpu $gpu_id --cal_pro True --category bottle
+python train.py --gpu $gpu_id --cal_pro True --category cable
+python train.py --gpu $gpu_id --cal_pro True --category capsule
+python train.py --gpu $gpu_id --cal_pro True --category carpet
+python train.py --gpu $gpu_id --cal_pro True --category grid
+python train.py --gpu $gpu_id --cal_pro True --category hazelnut
+python train.py --gpu $gpu_id --cal_pro True --category leather
+python train.py --gpu $gpu_id --cal_pro True --category metal_nut
+python train.py --gpu $gpu_id --cal_pro True --category pill
+python train.py --gpu $gpu_id --cal_pro True --category screw
+python train.py --gpu $gpu_id --cal_pro True --category tile
+python train.py --gpu $gpu_id --cal_pro True --category toothbrush
+python train.py --gpu $gpu_id --cal_pro True --category transistor
+python train.py --gpu $gpu_id --cal_pro True --category wood
+python train.py --gpu $gpu_id --cal_pro True --category zipper
